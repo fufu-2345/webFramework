9b158c09962848ce7d55194f6825db0b1b6a66b8
fufu-2345 <mr.kittisak.ploysumrit@gmail.com>
2026-01-28 02:19:49 +0700

    fix enum

--- apps/back/dbControl/createTable.js
+++ apps/back/dbControl/createTable.js
@@ -16,10 +16,10 @@ async function setupDatabase() {
         const User = `
             CREATE TABLE IF NOT EXISTS \`User\` (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                name VARCHAR(255) NOT NULL UNIQUE,
+                username VARCHAR(255) NOT NULL UNIQUE,
                 email VARCHAR(255) NOT NULL UNIQUE,
                 password VARCHAR(255) NOT NULL,
-                role ENUM('user', 'admin') DEFAULT 'user'
+                role VARCHAR(255) DEFAULT 'user'
             )
         `;
 
