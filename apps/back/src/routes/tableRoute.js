8a0cdba055777fd30cee2269ba297e996b48026a
Fomkiki <fomsupakorn17@gmail.com>
2026-02-03 04:05:35 +0700

    myTablesPage

--- apps/back/src/routes/tableRoute.js
+++ apps/back/src/routes/tableRoute.js
@@ -6,5 +6,7 @@ router.get('/', controller.getTables);
 
 router.get("/available", controller.getAvailableTime);
 router.post("/reserve", controller.reserveTable);
+router.get("/mytable/:userID", controller.getMyReservedTables);
+
 
 module.exports = router;
